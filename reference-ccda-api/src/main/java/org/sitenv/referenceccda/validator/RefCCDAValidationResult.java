e6e93177fe6ecdc00b03e536ce70248fdea554f7
Brian Harris <sendbrianemail@gmail.com>
2015-09-01 14:42:42 -0400

    Added enum for error type.

--- reference-ccda-api/src/main/java/org/sitenv/referenceccda/validator/RefCCDAValidationResult.java
+++ reference-ccda-api/src/main/java/org/sitenv/referenceccda/validator/RefCCDAValidationResult.java
@@ -1,23 +1,14 @@
 package org.sitenv.referenceccda.validator;
 
+import org.sitenv.referenceccda.validator.enums.ErrorType;
+
 public class RefCCDAValidationResult {
-	
-	static public String CCDA_IG_CONFORMANCE_ERROR = "CCDA_IG_ERROR";
-	static public String CCDA_IG_CONFORMANCE_WARN = "CCDA_IG_WARN";
-	static public String CCDA_IG_CONFORMANCE_INFO = "CCDA_IG_INFO";
-	static public String CCDA_VOCAB_CONFORMANCE_ERROR = "CCDA_VOCAB_ERROR";
-	static public String CCDA_VOCAB_CONFORMANCE_WARN = "CCDA_VOCAB_WARN";
-	static public String CCDA_VOCAB_CONFORMANCE_INFO = "CCDA_VOCAB_INFO";
-	static public String REF_CCDA_ERROR = "REF_CCDA_ERROR";
-	static public String REF_CCDA_WARN = "REF_CCDA_WARN";
-	static public String REF_CCDA_INFO = "REF_CCDA_INFO";
-	
-	//Common Error information
+
+	// Common Error information
 	private String errorDescription;
-	private String errorType;
+	private ErrorType errorType;
 	private String xPath;
-	
-	
+
 	// Only valid for Vocabulary testing
 	private String expectedCodeSystem;
 	private String actualCodeSystem;
@@ -26,115 +17,91 @@ public class RefCCDAValidationResult {
 	private String expectedDisplayName;
 	private String actualDisplayName;
 	private String expectedValueSet;
-	
-	public RefCCDAValidationResult(String errorDesc, String xpath, String errType)
-	{
+
+	public RefCCDAValidationResult(String errorDesc, String xpath, ErrorType ccdaIgConformanceError) {
 		errorDescription = errorDesc;
 		xPath = xpath;
-		errorType = errorType;
+		errorType = ccdaIgConformanceError;
 	}
-	
-	
+
 	public String getErrorDescription() {
 		return errorDescription;
 	}
-
 
 	public void setErrorDescription(String errorDescription) {
 		this.errorDescription = errorDescription;
 	}
 
-
-	public String getErrorType() {
+	public ErrorType getErrorType() {
 		return errorType;
 	}
 
-
-	public void setErrorType(String errorType) {
+	public void setErrorType(ErrorType errorType) {
 		this.errorType = errorType;
 	}
-
 
 	public String getxPath() {
 		return xPath;
 	}
 
-
 	public void setxPath(String xPath) {
 		this.xPath = xPath;
 	}
-
 
 	public String getExpectedCodeSystem() {
 		return expectedCodeSystem;
 	}
 
-
 	public void setExpectedCodeSystem(String expectedCodeSystem) {
 		this.expectedCodeSystem = expectedCodeSystem;
 	}
-
 
 	public String getActualCodeSystem() {
 		return actualCodeSystem;
 	}
 
-
 	public void setActualCodeSystem(String actualCodeSystem) {
 		this.actualCodeSystem = actualCodeSystem;
 	}
-
 
 	public String getExpectedCode() {
 		return expectedCode;
 	}
 
-
 	public void setExpectedCode(String expectedCode) {
 		this.expectedCode = expectedCode;
 	}
-
 
 	public String getActualCode() {
 		return actualCode;
 	}
 
-
 	public void setActualCode(String actualCode) {
 		this.actualCode = actualCode;
 	}
-
 
 	public String getExpectedDisplayName() {
 		return expectedDisplayName;
 	}
 
-
 	public void setExpectedDisplayName(String expectedDisplayName) {
 		this.expectedDisplayName = expectedDisplayName;
 	}
-
 
 	public String getActualDisplayName() {
 		return actualDisplayName;
 	}
 
-
 	public void setActualDisplayName(String actualDisplayName) {
 		this.actualDisplayName = actualDisplayName;
 	}
-
 
 	public String getExpectedValueSet() {
 		return expectedValueSet;
 	}
 
-
 	public void setExpectedValueSet(String expectedValueSet) {
 		this.expectedValueSet = expectedValueSet;
 	}
 
-
-	
-	
 }
--- reference-ccda-api/src/main/java/org/sitenv/referenceccda/validator/ReferenceCCDAValidator.java
+++ reference-ccda-api/src/main/java/org/sitenv/referenceccda/validator/ReferenceCCDAValidator.java
@@ -1,42 +1,39 @@
 package org.sitenv.referenceccda.validator;
 
-import org.openhealthtools.mdht.uml.cda.consol.ConsolPackage;
-import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
-
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.emf.common.util.Diagnostic;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolPackage;
 import org.openhealthtools.mdht.uml.cda.util.CDADiagnostic;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 import org.openhealthtools.mdht.uml.cda.util.ValidationResult;
+import org.sitenv.referenceccda.validator.enums.ErrorType;
 
 public class ReferenceCCDAValidator {
-	
-	public static ArrayList<RefCCDAValidationResult> validateCCDAWithReferenceFileName(
-			String validationObjective,
-			String referenceFileName,
-			String ccdaFile)
-	{
-		// create a validation result object to collect diagnostics produced during validation
+
+	public static ArrayList<RefCCDAValidationResult> validateCCDAWithReferenceFileName(String validationObjective,
+			String referenceFileName, String ccdaFile) {
+		// create a validation result object to collect diagnostics produced
+		// during validation
 		ValidationResult result = new ValidationResult();
 		ConsolPackage.eINSTANCE.eClass();
-		
+
 		try {
-			
-			//Identify the objectives and check for the document type
-			
+
+			// Identify the objectives and check for the document type
+
 			// Check if all files are C-CDA compliant
 			InputStream in = IOUtils.toInputStream(ccdaFile, "UTF-8");
-			ClinicalDocument clinicalDocument = CDAUtil.load(in, result);
-			
+			CDAUtil.load(in, result);
+
 			// If there are no schema errors, check for Vocabulary errors
-			
-			// If there are no vocab errors, then check for reference CCDA errors
-					
+
+			// If there are no vocab errors, then check for reference CCDA
+			// errors
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,33 +41,30 @@ public class ReferenceCCDAValidator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		ArrayList<RefCCDAValidationResult> results = new ArrayList<RefCCDAValidationResult>();
-		
+
 		for (Diagnostic diagnostic : result.getErrorDiagnostics()) {
-		   
+
 			CDADiagnostic diag = new CDADiagnostic(diagnostic);
-			results.add(new RefCCDAValidationResult(
-							diag.getMessage(), diag.getPath(), RefCCDAValidationResult.CCDA_IG_CONFORMANCE_ERROR));
-			
-		  }
+			results.add(new RefCCDAValidationResult(diag.getMessage(), diag.getPath(), ErrorType.CCDA_IG_CONFORMANCE_ERROR));
+
+		}
 
 		for (Diagnostic diagnostic : result.getWarningDiagnostics()) {
-		
+
 			CDADiagnostic diag = new CDADiagnostic(diagnostic);
-			results.add(new RefCCDAValidationResult(
-							diag.getMessage(), diag.getPath(), RefCCDAValidationResult.CCDA_IG_CONFORMANCE_WARN));
-			
-		  }
-		
+			results.add(new RefCCDAValidationResult(diag.getMessage(), diag.getPath(), ErrorType.CCDA_IG_CONFORMANCE_WARN));
+
+		}
+
 		for (Diagnostic diagnostic : result.getInfoDiagnostics()) {
-			
+
 			CDADiagnostic diag = new CDADiagnostic(diagnostic);
-			results.add(new RefCCDAValidationResult(
-							diag.getMessage(), diag.getPath(), RefCCDAValidationResult.CCDA_IG_CONFORMANCE_INFO));
-			
-		  }
-		
+			results.add(new RefCCDAValidationResult(diag.getMessage(), diag.getPath(), ErrorType.CCDA_IG_CONFORMANCE_INFO));
+
+		}
+
 		return results;
 	}
 
