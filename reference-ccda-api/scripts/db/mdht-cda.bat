c62344f6e5944973c0af0ec87e4949d133aef344
drbgfc <drb.gfc@comcast.net>
2016-04-07 13:22:51 -0400

    Add local jar deployment scripts

---
+++ reference-ccda-api/scripts/db/mdht-cda.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -DlocalRepositoryPath=mdht-vocab-repo -DcreateChecksum=true -Dpackaging=jar -Dfile=C:\temp\builds\r2\new\runtime\mdht\org.openhealthtools.mdht.uml.cda-2.5.19.201603232017.jar -DgroupId=org.mdht.dependencies -DartifactId=org.openhealthtools.mdht.uml.cda -Dversion=2.5.19.201603232017
