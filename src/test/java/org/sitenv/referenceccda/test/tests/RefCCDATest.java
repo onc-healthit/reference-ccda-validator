ac9e451e00740aee8ccecb5742f395c9b355b0cb
Dan Brown <dbrown.se@gmail.com>
2022-08-25 17:20:36 -0400

    sync MDHT plugins with models 3.0.0.202208250500

--- src/test/java/org/sitenv/referenceccda/test/tests/RefCCDATest.java
+++ src/test/java/org/sitenv/referenceccda/test/tests/RefCCDATest.java
@@ -505,12 +505,11 @@ public class RefCCDATest extends ReferenceValidationTester implements Validation
 	
 	@Test
 	public void checkPregnancyCodes() {
-		
 		List<RefCCDAValidationResult> results = validateDocumentAndReturnResults(
 				convertCCDAFileToString(CCDA_FILES[TWO_MEGS]));
 		results = getMDHTErrorsFromResults(results);
-		failIfIssueIsInResults(results, ValidationResultType.CCDA_MDHT_CONFORMANCE_ERROR, "2.16.840.1.113762.1.4.1099.2");	
-		
+		failIfIssueIsInResults(results, ValidationResultType.CCDA_MDHT_CONFORMANCE_ERROR,
+				"2.16.840.1.113762.1.4.1099.2");
 	}
 
 	@Test
